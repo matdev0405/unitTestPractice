d21625b008128dfa8503f1f9a568c750f88bea6b
matdev0405 <77271966+matdev0405@users.noreply.github.com>
2022-05-25 14:02:56 +0800

    unitTest Practice

--- unitTestPractice/ViewController.swift
+++ unitTestPractice/ViewController.swift
@@ -8,10 +8,38 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let nameLabel : UILabel = {
+        let a = UILabel(frame: CGRect(x: 0, y: 0, width: 100, height: 20))
+        a.translatesAutoresizingMaskIntoConstraints = false
+        a.text = "label1"
+        return a
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.addSubview(nameLabel)
+        nameLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        nameLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        
+    }
+    
+    func plus(_ a: Int?, _ b: Int?) -> Int? {
+            guard let numberA = a, let numberB = b else { return nil }
+            return numberA + numberB
+        }
+    
+    /// 改變 Label 的文字
+    /// - Parameter wording: _
+    func changeWroding(_ wording: String) {
+        nameLabel.text = wording
+    }
+    
+    func loopSomething() {
+        for i in 0...10000 {
+            print("i:\(i)")
+        }
     }
 
 
--- unitTestPracticeTests/unitTestPracticeTests.swift
+++ unitTestPracticeTests/unitTestPracticeTests.swift
@@ -8,22 +8,86 @@
 import XCTest
 @testable import unitTestPractice
 
+/**
+    單元測試主要三大方向:
+        1. 測試沒有跟UI連動的
+        2. 測試與UI連動的
+        3. 測試非同步的api回傳狀態
+ */
+
 class unitTestPracticeTests: XCTestCase {
+    
+    var vc : ViewController!
+    var urlSession: URLSession!
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        vc = ViewController()
+        urlSession = URLSession(configuration: .default)
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        urlSession = nil
+    }
+    
+    // MARK: - 測試沒有與UI連動的
+    
+    func testPlusWithValue() {
+        let a = 3
+        let b = 2
+        
+        let result = vc.plus(a, b)
+        
+        XCTAssertEqual(result, 5, "Result is wrong")
+    }
+//
+    func testPlusWithoutValue() {
+        let a = 3
+
+        let result = vc.plus(a, nil)
+
+        XCTAssertNil(result, "Result is not nil")
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    // MARK: - 測試與UI連動的
+    
+    func testLabelText() {
+        
+        let text = "I'm Matthew"
+        
+        vc.changeWroding(text)
+        
+        XCTAssertEqual(text, vc.nameLabel.text)
+    }
+    
+    // MARK: - 測試非同步的api回傳狀態
+    func testAsynchronousAPI() {
+            // Given
+            var statusCode: Int?
+            var responseError: Error?
+            let url = URL(string: "https://www.google.com")
+            let promise = expectation(description: "Invalid status code.") // 若是執行錯誤則會 log 出來的訊息
+            
+            // When
+            let dataTask = urlSession.dataTask(with: url!) { data, response, error in
+              statusCode = (response as? HTTPURLResponse)?.statusCode
+              responseError = error
+              promise.fulfill() // 表示非同步的任務已經結了，並告知 Wait() 可以結束了。表示可以繼續往下執行待驗證的數值。
+            }
+            
+            dataTask.resume()
+            wait(for: [promise], timeout: 8) // 設定被測試的 API 的 Timeout 時間
+                    
+            // Then
+            XCTAssertNil(responseError, "Response is error.")
+            XCTAssertEqual(statusCode, 200, "Invalid status code.")
+        }
+    
+    func testLoopPerformance() {
+        self.measure { //效能測試
+            self.vc.loopSomething()
+        }
     }
 
     func testPerformanceExample() throws {
